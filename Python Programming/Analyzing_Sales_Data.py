9b15e41ecdf41e91d72ba78e044fee7884bb1473
maypn <109836477+maypn@users.noreply.github.com>
2022-08-02 11:38:01 +0700

    Update and rename Analyzing Sales Data (Pandas) to Analyzing_Sales_Data.py

---
+++ Python Programming/Analyzing_Sales_Data.py
@@ -0,0 +1,12 @@
+# import data
+import pandas as pd 
+df = pd.read_csv("sample-store.csv")
+
+# preview top 5 rows
+df.head()
+
+# shape of dataframe
+df.shape
+
+# see data frame information using
+df.info()
